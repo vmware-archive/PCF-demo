ad81931b88e653731ebdece43bd3ab54aa385877
patrickcrocker <patrick.crocker@gmail.com>
2016-02-13 05:38:50 -0600

    refactored to process manifest.yml for cf integration test deployment

---
+++ ci/tasks/process-manifest.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# required
+inputManifest=  outputManifest=
+
+# optional
+host=$CF_MANIFEST_HOST  path=
+
+while [ $# -gt 0 ]; do
+  case $1 in
+    -i | --input-manifest )
+      inputManifest=$2
+      shift
+      ;;
+    -o | --output-manifest )
+      outputManifest=$2
+      shift
+      ;;
+    -n | --host )
+      host=$2
+      shift
+      ;;
+    -p | --path )
+      path=$2
+      shift
+      ;;
+    * )
+      echo "Unrecognized option: $1" 1>&2
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+if [ ! -f "$inputManifest" ]; then
+  echo "missing input manifest!"
+  exit 1
+fi
+
+if [ -z "$outputManifest" ]; then
+  echo "missing output manifest!"
+  exit 1
+fi
+
+cp $inputManifest $outputManifest
+
+if [ ! -z "$host" ]; then
+  sed -i "s|host: .*$|host: ${host}|g" $outputManifest
+fi
+
+if [ ! -z "$path" ]; then
+  if [ -d "$path" ]; then
+    path=`find $path -name '*.war'`
+  fi
+  sed -i -- "s|path: .*$|path: ${path}|g" $outputManifest
+fi
+
+cat $outputManifest
